24b6f1835e3ad5255490520ab5f2da193d5bd49c
HenryTene <henry_tene@hotmail.com>
2025-10-27 10:52:27 -0500

    :sparkles:  crear componente Category con props

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,28 @@
+import Categoria from "./Categoria";
 import { categorias } from "../data/categorias";
 
 export default function Sidebar() {
   return (
     <aside className="md:w-72">
       <div className="p-4">
-        <img className="w-40" src="img/logo.svg" alt="" />
+        <img className="w-40" src="img/logo.svg" alt="Imagen Logo" />
       </div>
       <div className="mt-10">
         {categorias.map(categoria => (
-          <p>{categoria.nombre}</p>
+          <Categoria
+            categoria={categoria}
+            key={categoria.id}
+          
+          />
         ))}
-         
-
+      </div>
+      <div className="my-5 px-5">
+        <button 
+          type="button"
+          className="text-center bg-red-500 w-full hover:bg-red-600   p-3 font-bold text-white rounded truncate"
+          >
+          Cancelar Orden
+        </button>
       </div>
     </aside>
   );
