9bc048158df2688a0f5f41a06703e24e49be90c8
HenryTene <henry_tene@hotmail.com>
2025-11-01 10:38:01 -0500

    :sparkles: crear context para estado de la aplicación

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './assets/router.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { QuioscoProvider } from "./context/QuioscoProvider.jsx";
+import router from "./assets/router.jsx";
+import "./index.css";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <QuioscoProvider>
+      <RouterProvider router={router} />
+    </QuioscoProvider>
+  </StrictMode>
+);
