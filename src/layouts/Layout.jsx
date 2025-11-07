de12483bcb89c11f7a1e7c47b877fbecf49c4994
HenryTene <henry_tene@hotmail.com>
2025-11-06 19:35:35 -0500

    :sparkles: configurar React Modal en aplicación

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,21 +1,41 @@
 import { Outlet } from "react-router-dom";
+import Modal from "react-modal";
 import Sidebar from "../components/Sidebar";
 import Resumen from "../components/Resumen";
 import useQuiosco from "../hooks/useQuiosco";
 
-export default function Layout() {
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
-  const { modal } = useQuiosco();
+export default function Layout() {
+  const { modal, handlleClickModal } = useQuiosco();
 
   console.log(modal);
 
   return (
-    <div className="md:flex md:min-h-screen">
-      <Sidebar />
-      <main className="flex-1 h-screen overflow-y-scroll bg-gray-100 p-10">
-        <Outlet />
-      </main>
-      <Resumen />
-    </div>
+    <>
+      <div className="md:flex md:min-h-screen">
+        <Sidebar />
+        <main className="flex-1 h-screen overflow-y-scroll bg-gray-100 p-10">
+          <Outlet />
+        </main>
+        <Resumen />
+      </div>
+
+      {modal && (
+        <Modal isOpen={modal} style={customStyles} ariaHideApp={false}>
+          <p>Hola desde el modal</p>
+          <button onClick={handlleClickModal}>Cerrar</button>
+        </Modal>
+      )}
+    </>
   );
 }
