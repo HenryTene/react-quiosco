a770ac1feea7ae3f52085a4b6d0ded8c2c760ca8
HenryTene <henry_tene@hotmail.com>
2025-11-13 15:45:03 -0500

    :sparkles: actualizar modal para modo edición de orden

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useQuiosco from "../hooks/useQuiosco";
 import { formatearDinero } from "../helpers";
 
 export default function ModalProducto() {
-  const { producto, handleClickModal, handleAgregarPedido } = useQuiosco();
+  const { producto, handleClickModal, handleAgregarPedido, pedido } =
+    useQuiosco();
   const [cantidad, setCantidad] = useState(1);
+  const [edicion, setEdicion] = useState(false);
+
+  useEffect(() => {
+    if (pedido.some((pedidoState) => pedidoState.id === producto.id)) {
+      const productoEdicion = pedido.filter(
+        (pedidoState) => pedidoState.id === producto.id
+      )[0];
+      setCantidad(productoEdicion.cantidad);
+      setEdicion(true);
+    }
+  }, [pedido]);
 
   return (
     <div className="md-flex items-center gap-10">
@@ -41,14 +53,13 @@ export default function ModalProducto() {
         </p>
 
         <div className="flex gap-4 mt-5">
-          <button 
+          <button
             type="button"
-             
             onClick={() => {
-              if (cantidad <= 1) return 
-              setCantidad(cantidad - 1)
-              }}
-              >
+              if (cantidad <= 1) return;
+              setCantidad(cantidad - 1);
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -65,10 +76,13 @@ export default function ModalProducto() {
             </svg>
           </button>
           <p className="text-3xl">{cantidad}</p>
-          <button type="button" onClick={() => {
-              if (cantidad >= 50) return 
-              setCantidad(cantidad + 1)
-              }}>
+          <button
+            type="button"
+            onClick={() => {
+              if (cantidad >= 50) return;
+              setCantidad(cantidad + 1);
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -90,11 +104,11 @@ export default function ModalProducto() {
           type="button"
           className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5  text-white font-bold  uppercase  rounded"
           onClick={() => {
-            handleAgregarPedido({...producto, cantidad})
-            handleClickModal()
+            handleAgregarPedido({ ...producto, cantidad });
+            handleClickModal();
           }}
         >
-          Añadir al pedido
+          {edicion ? "Guardar Cambios" : "Añadir al pedido"}
         </button>
       </div>
     </div>
