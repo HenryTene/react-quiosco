4ff7e4932bf2e7ce5d824d0c6d7f1dec71c33b88
HenryTene <henry_tene@hotmail.com>
2025-11-20 11:17:35 -0500

    :sparkles: deshabilitar botón si pedido está vacío

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -2,7 +2,12 @@ import { formatearDinero } from "../helpers";
 import useQuiosco from "../hooks/useQuiosco";
 import ResumenProducto from "./ResumenProducto";
 export default function Resumen() {
-  const { pedido, total} = useQuiosco();
+  const { pedido, total } = useQuiosco();
+
+  const comprobarPedido = () => {
+    return pedido.length === 0;
+  };
+
   return (
     <aside className="w-72 h-screen overflow-y-scroll p-5">
       <h1 className="text-4xl font-bold text-center">Mi Pedido</h1>
@@ -13,18 +18,26 @@ export default function Resumen() {
         {pedido.length === 0 ? (
           <p className="text-center text-2xl">No hay elementos en tu pedido</p>
         ) : (
-          pedido.map(producto => (<ResumenProducto 
-          key={producto.id} producto={producto}
-          />))
+          pedido.map((producto) => (
+            <ResumenProducto key={producto.id} producto={producto} />
+          ))
         )}
       </div>
-      <p className="text-xl mt-10">Total a pagar: {""} {formatearDinero(total)}</p>
+      <p className="text-xl mt-10">
+        Total a pagar: {""} {formatearDinero(total)}
+      </p>
       <form className="w-full">
         <div className="mt-5">
           <input
-            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase  font-bold  text-white text-center w-full cursor-pointer"
             type="submit"
+            className={`${
+              comprobarPedido()
+                ? "bg-indigo-100"
+                : "bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase  font-bold  text-white text-center w-full  cursor-pointer"
+            }
+                 `}
             value="Confirmar Pedido"
+            disabled={comprobarPedido()}
           />
         </div>
       </form>
