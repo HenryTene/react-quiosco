6d1276c9d8e32e4b63f2bb370b9bb0267a4af85c
HenryTene <henry_tene@hotmail.com>
2025-12-01 13:13:01 -0500

    :sparkles: instalar UseSWR para estado del servidor

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,24 +1,34 @@
-import { productos as data } from "../data/productos";
+import useSWR from "swr";
 import Producto from "../components/Producto";
 import useQuiosco from "../hooks/useQuiosco";
+import clientAxios from "../config/axios";
 
 export default function Inicio() {
-  
-const { categoriaActual } = useQuiosco();
+  const { categoriaActual } = useQuiosco();
 
-const productos = data.filter( producto => producto.categoria_id === categoriaActual.id );
+  //consulta SWR
+  const fetcher = () => clientAxios("/api/productos").then((data) => data.data);
+
+  const { data, error, isLoading } = useSWR("/api/productos", fetcher, {
+    refreshInterval: 1000,
+  });
+
+  if (isLoading) return <p>Cargando...</p>;
+
+  const productos = data.data.filter(
+    (producto) => producto.categoria_id === categoriaActual.id
+  );
 
   return (
     <>
       <h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
-      <p className="text-2xl my-10 ">Elige y personaliza tu pedido a continuación</p>
+      <p className="text-2xl my-10 ">
+        Elige y personaliza tu pedido a continuación
+      </p>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ">
-          {productos.map(producto =>(
-            <Producto
-              key={producto.imagen}
-              producto={producto}
-            />
-          ) )}
+        {productos.map((producto) => (
+          <Producto key={producto.imagen} producto={producto} />
+        ))}
       </div>
     </>
   );
